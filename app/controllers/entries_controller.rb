0795e9fbd407c86ee51bf86672024f1371240025
Greg Shannon <greg.shannon@kellogg.northwestern.edu>
2025-03-14 21:23:55 +0000

    jpg uploads

--- app/controllers/entries_controller.rb
+++ app/controllers/entries_controller.rb
@@ -1,20 +1,25 @@
 class EntriesController < ApplicationController
+  before_action :require_login
+
+  def new
+    @entry = Entry.new
+  end
+
   def create
     @entry = Entry.new(entry_params)
-    @entry.place_id = params[:place_id]  # Explicitly set place_id
-    @entry.user_id = session[:user_id]   # Assign the logged-in user
+    @entry.user_id = session[:user_id] # Associate entry with the logged-in user
 
     if @entry.save
       redirect_to place_path(@entry.place_id), notice: "Entry created successfully!"
     else
-      render :new, status: :unprocessable_entity
+      render :new
     end
   end
 
   private
 
   def entry_params
-    params.require(:entry).permit(:title, :description, :occurred_on)
+    params.require(:entry).permit(:title, :description, :occurred_on, :place_id, :image)
   end
 end
 
